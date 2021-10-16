5e23a2b3e23e560f92ecd9bed17e61229a9be37a
Pralay Das <pralay.ceremorphic@gmail.com>
2021-10-16 21:12:10 +0530

    and file completed

--- and.py
+++ and.py
@@ -1,5 +1,5 @@
 from utils.model import Perceptron
-from utils.all_utils import prepare_data
+from utils.all_utils import prepare_data, save_model, save_plot
 import pandas as pd
 import numpy as np
 
@@ -10,7 +10,7 @@ AND ={
     "y" :[0,0,0,1]
 }
 df = pd.DataFrame(AND)
-
+print(df)
 
 X , y = prepare_data(df)
 print(f"X shape :{X.shape}, y shape : {y.shape}")
@@ -20,3 +20,6 @@ model = Perceptron(eta = ETA, epochs=EPOCH)
 model.fit(X,y)
 
 _ = model.total_loss()
+
+save_model(model, filename="and.model")
+save_plot(df,file_name="and.png", model=model)
